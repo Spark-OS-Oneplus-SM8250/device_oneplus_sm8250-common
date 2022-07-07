a62888968a3da139d9056c888b70724485517bdc
HELLBOY017 <abhaygill017@gmail.com>
2022-07-08 00:08:15 +0530

    sm8250-common: build android.frameworks.sensorservice@1.0 HAL
    Signed-off-by: DawfukFR <dawfukfr@gmail.com>

--- common.mk
+++ common.mk
@@ -394,6 +394,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal_oplus \
     libsensorndkbridge \
     sensors.oplus

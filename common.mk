c99a8b23571716a9107f354e9babc952fcce47d7
Mathieu Chartier <>
2022-11-20 13:19:25 +0530

    sm8250-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- common.mk
+++ common.mk
@@ -392,6 +392,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \

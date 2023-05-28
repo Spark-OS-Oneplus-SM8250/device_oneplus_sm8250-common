3d8cdd7e2718cf51338013b992fdbd9bfe4e11c6
Devang Chaudhary <devangsingh369@gmail.com>
2023-05-28 21:38:29 +0530

    Revert "sm8250-common: RIP `remove_packages`"
    * WHY LUK, JUST WHY!
    
    This reverts commit 7ad5bbf53f5b0bcc0d876925ac7b8005cf53e3c1.
    
    Signed-off-by: Devang Chaudhary <devangsingh369@gmail.com>
    Signed-off-by: DawfukFR <DawfukFR@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX MusicFX Aperture Gallery Gallery2 DevCamera Car CalendarGooglePrebuilt Drive Maps Photos PrebuiltGmail Videos YouTube AndroidAutoStubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := NfcNci AudioFX MusicFX Aperture Gallery Gallery2 DevCamera Car CalendarGooglePrebuilt Drive Maps Photos PrebuiltGmail Videos YouTube AndroidAutoStubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
